1580e2d9afc929aa41af5be99c314fd88a2d59c0
syranez <github@minaga-church.de>
2019-03-11 19:00:42 +0100

    Example: Update shows only milestones.

--- example/formatter/update.js
+++ example/formatter/update.js
@@ -6,33 +6,17 @@ class Progress {
 
         event.diff.forEach(change => {
             switch (change.key) {
-            case 'place':
-                if (event.switchedBranch()) {
-                    changes.push(`ist von ${change.oldValue} nach ${change.newValue} gegangen`);
-                }
-                break;
             case 'milestone':
                 changes.push(`hat einen Meilenstein geschafft: "${change.newValue}"`);
                 break;
-            case 'title':
-                changes.push(`ist nun ein "${change.newValue}"`);
-                break;
-            case 'xl':
-                changes.push(`ist nun Level ${change.newValue}`);
-                break;
-            case 'idle_time':
-                break;
-            case 'spectator_count':
-                break;
             default:
-                console.warn('Unhandled: ' + change.key);
+                /* ignore all other */
             }
         });
 
         if (changes.length === 0) {
             return null;
         }
-
 
         return `[${event.serverInfo.shortcut}] ${event.username} ${changes.join(' und ')}.`;
     }
